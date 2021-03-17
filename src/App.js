131f833f0385584d75982619f40f8339e5ae4a53
Nana  BAKALAFOUA <nbakalafoua@gmail.com>
2021-03-17 12:46:18 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import CreatePokemon from './Components/CreatePokemon';
+import DisplayPokemon from './Components/DisplayPokemon';
+import AppContextProvider from './context/AppContextProvider';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    return (
+        <AppContextProvider>
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/display" component={DisplayPokemon}></Route>
+                    <Route path="/" component={CreatePokemon}></Route>
+                </Switch>
+            </BrowserRouter>
+        </AppContextProvider>
+
+    )
 }
 
 export default App;
